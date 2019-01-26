ae6cf9e203230fb110395e01eea126784aa615a5
Ryan <ryandoyle@me.com>
2019-01-26 14:13:55 -0800

    Doing work

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,30 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+  
+  addFish = (fish) => {
+    console.log('Adding a fish!');
+    // copy the state
+    const fishes = {...this.state.fishes};
+    // add new fish to the copy of state with date as the uniqe id
+    fishes[`fish${Date.now()}`] = fish;
+    //set new fish object to the state
+    this.setState({
+      fishes: fishes
+    })
+  }
+
   render() {
     return (
       <div className='catch-of-the-day'>
         <div className='menu'>
           <Header tagline='Fresh Seafood Market'/>
         </div>
-        <Inventory />
+        <Inventory addFish={this.addFish} />
         <Order />
       </div>
     )
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => { //es6 alternate syntax so you can do without the constructor .bind stuff
+    event.preventDefault();
+    const storeName = this.myInput.value.value;
+    console.log(this.myInput.value.value);
+    // pushes current state to the given url, and react router re-renders
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <p>
-          <form className="store-selector">
-            <h2>Please Enter a Store</h2>
-            <input type='text' required placeholder='Store Name' />
-            <button type='submit'>Visit Store -></button>
-          </form>
-        </p>
-      </React.Fragment>
+      <form className='store-selector' onSubmit={this.goToStore}>
+        <h2>Please Enter a Store</h2>
+        <input
+          type='text'
+          ref={this.myInput}
+          required
+          placeholder='Store Name'
+          defaultValue={getFunName()}
+        />
+        <button type='submit'>Visit Store -></button>
+      </form>
     )
   }
 }
